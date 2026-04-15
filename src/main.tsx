2dde3482f3c2201c271bf3f086db4eb38cfa6ad8
yesekayumpa <yesekayumpab@gmail.com>
2026-04-15 16:45:05 +0000

    Remove Google OAuth configuration to fix deployment error

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || 'demo-client-id'}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </StrictMode>,
 );
